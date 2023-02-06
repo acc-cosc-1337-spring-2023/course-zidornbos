be138a9cd2107662eb48c006ecde9b4db334822f
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-02-06 15:41:07 +0000

    expressions

--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,17 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "expressions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test operator precedence 1")
+{
+	REQUIRE(operator_precedence_1(12, 6, 3) == 14);
+}
+
+TEST_CASE("Test operator precedence 2")
+{
+	REQUIRE(operator_precedence_2(12, 6, 3) == 6);
 }
