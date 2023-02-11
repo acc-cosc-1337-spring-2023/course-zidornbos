04f879d2dda3719d4cf7c9c3ef02389d3cff7f79
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-02-11 23:58:17 +0000

    Zi_Cafe

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,36 @@
 ﻿//write include statements
-
+#include<iostream>
+#include "hwexpressions.h"
 
 //write namespace using statement for cout
 
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
+
 int main()
 {
-	
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+
+	cout << "Enter meal amount: ";
+	cin >> meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout << "Enter amount you would like to tip: ";
+	cin >> tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+	total = meal_amount + tax_amount + tip_amount;
+
+	cout << "Zi Cafe Receipt: \n";
+	cout << "Meal Amount: " << meal_amount << "\n";
+	cout << "Sales Tax: " << tax_amount << "\n";
+	cout << "Tip Amount: " << tip_amount << "\n";
+	cout << "Total: " << total << "\n";
 
 	return 0;
 }
