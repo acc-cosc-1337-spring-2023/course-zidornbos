263aacf3ee15d2eb6e00adc005bc6be348a2d63b
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-02-11 22:58:33 +0000

    1

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -8,6 +8,11 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
-
+double get_sales_tax_amount(double meal_amount)
+{
+const double tax_rate = 0.0675;
+double sales_tax = meal_amount * tax_rate;
+return sales_tax;
+}
 
 
