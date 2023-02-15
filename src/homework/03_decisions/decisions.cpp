e8d97de9054ef9d96b2599aa78d4014768d7e202
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-02-15 23:16:27 +0000

    using if test passed

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -4,3 +4,34 @@
 using std::string;
 //Write code for function(s) code here
 
+string get_letter_grade_using_if(int grade)
+{
+    string result;
+
+    if(grade >= 90 && grade <= 100)
+    {
+        result = 'A';
+    }
+    else if(grade >= 80 && grade <= 89)
+    {
+        result = 'B';
+    }
+    else if(grade >= 70 && grade <= 79)
+    {
+        result = 'C';
+    }
+    else if(grade >= 60 && grade <= 69)
+    {
+        result = 'D';
+    }
+    else if(grade >= 00 && grade <= 59)
+    {
+        result = 'F';
+    }
+    else
+    {
+        result = "Out of Range";
+    }
+
+    return result;
+}
