81438184a2fa1305f067b4e69be2cea670970719
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-02-15 18:04:59 +0000

    Examples

--- test/examples/03_conditionals/03_conditionals_tests.cpp
+++ test/examples/03_conditionals/03_conditionals_tests.cpp
@@ -1,6 +1,18 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "if_else_if.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test get generation")
+{
+	REQUIRE(get_generation(2023) == "Invalid Year");
+	REQUIRE(get_generation(2010) == "Centennial");
+	REQUIRE(get_generation(1990) == "Millennial");
+	REQUIRE(get_generation(1970) == "Generation X");
+	REQUIRE(get_generation(1950) == "Baby Boomer");
+	REQUIRE(get_generation(1930) == "Silent Generation");
+	REQUIRE(get_generation(1900) == "Invalid Year");
 }
