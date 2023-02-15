26dffb19a2ce5d159404deef00b9c2d9372e6702
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-02-15 18:13:23 +0000

    Else if 1

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,6 @@
 //write include statement for decisions header
+#include "decisions.h"
 
-
+using std::string;
 //Write code for function(s) code here
+
--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,11 +1,11 @@
 //write include required statements
-
+#include<string>
 
 //Write a function(s) prototype  here
 
-int get_letter_grade_using_if();
+std::string get_letter_grade_using_if(int grade);
 
-int get_letter_grade_using_switch();
+std::string get_letter_grade_using_switch(int grade);
 
 
 
