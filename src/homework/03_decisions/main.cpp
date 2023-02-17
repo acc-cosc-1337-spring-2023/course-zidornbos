ea9323d99053a408c94e7cf868069a3f95a20272
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-02-17 18:07:46 +0000

    stuck on main

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -6,6 +6,15 @@ using std::cout; using std::cin;
 
 int main() 
 {
-	
+
+
+	cout << "Main Menu\n";
+	cout << "1-Letter grade using if\n";
+	cout << "2-Letter grade using switch\n";
+	cout << "3-Exit\n";
+	cout << "Enter your selection: ";
+	cin >> selection;
+
 	return 0;
-}
+
+	}
