5b1dbb7b47342e9f679f0b90884774a634c2c38d
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-02-18 01:54:56 +0000

    trying to fix out of range

--- test/homework/03_decisions/03_decisions_tests.cpp
+++ test/homework/03_decisions/03_decisions_tests.cpp
@@ -23,5 +23,4 @@ TEST_CASE("Test get letter grade using switch")
 	REQUIRE(get_letter_grade_using_switch(75) == "C");
 	REQUIRE(get_letter_grade_using_switch(65) == "D");
 	REQUIRE(get_letter_grade_using_switch(50) == "F");
-	REQUIRE(get_letter_grade_using_switch(0) == "F");
 }
