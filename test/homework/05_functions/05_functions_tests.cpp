e673503933f94ba7b0b21dd7c02e259fd268743d
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-05 05:20:03 +0000

    Homework 5 cpp complete and passed test

--- test/homework/05_functions/05_functions_tests.cpp
+++ test/homework/05_functions/05_functions_tests.cpp
@@ -1,7 +1,38 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "func.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+
+TEST_CASE("Verify GC-content function calculation")
+{
+	double result = get_gc_content("AGCTATAG");
+	REQUIRE(result == .375);
+}	
+
+TEST_CASE("Testing GC-content with different variable")
+{	
+	double second_result = get_gc_content("CGCTATAG");
+	REQUIRE(second_result == .50);
+}
+
+TEST_CASE("Verify reverse string function")
+{
+	std::string result = get_reverse_string("AGCTATAG");
+	REQUIRE(result =="GATATCGA");
+
+	std::string outcome = get_reverse_string("CGCTATAG");
+	REQUIRE(outcome == "GATATCGC");
+}
+
+TEST_CASE("Verify complement function")
+{
+	std::string result = get_dna_complement("AAAACCCGGT");
+	REQUIRE(result == "ACCGGGTTTT");
+
+	std::string outcome = get_dna_complement("CCCGGAAAAT");
+	REQUIRE(outcome == "ATTTTCCGGG");
+}
