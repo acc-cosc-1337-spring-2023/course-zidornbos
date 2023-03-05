cde46b5631d65929ffaaefd26f152abde1929b2a
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-05 05:20:45 +0000

    in progress examples

--- test/examples/08_classes/08_classes_tests.cpp
+++ test/examples/08_classes/08_classes_tests.cpp
@@ -1,6 +1,14 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "checking_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test checking get balance")
+{
+	CheckingAccount account;
+
+	REQUIRE(account.get_balance == 0);
 }
