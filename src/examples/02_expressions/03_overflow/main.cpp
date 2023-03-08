b790a122c330112b6369c65aa93ecada06d15a25
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-08 16:21:17 +0000

    expressions examples

--- src/examples/02_expressions/03_overflow/main.cpp
+++ src/examples/02_expressions/03_overflow/main.cpp
@@ -1,6 +1,22 @@
+#include<iostream>
+
+using std::cout;
 
 int main()
 {
+    auto num = 5;
+    auto num1 = 5.5;
+
+    cout<<"Size of num: "<<sizeof(num)<<"\n";
+    cout<<"Size of num1: "<<sizeof(num1)<<"\n";
+
+    num = 2147483647;
+    cout<<"Max value for int: "<<num<<"\n";
+
+    num += 1;
+    cout<<"Value of num int: "<<num<<"\n";
+    
+
 
     return 0;
 }
--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -2,6 +2,8 @@
 #include "catch.hpp"
 #include "expressions.h"
 #include "casting.h"
+#include "constants.h"
+
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -24,4 +26,9 @@ TEST_CASE("Test convert int to double")
 TEST_CASE("Test double to an int")
 {
 	REQUIRE(convert_double_to_int(10.5) == 10);
+}
+
+TEST_CASE("Verify get area of circle")
+{
+	REQUIRE(get_area_of_circle(10) == 314.159);
 }
