240dc2d23fe9faeac2f1d9c703e235f744229ff9
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-08 16:21:34 +0000

    bank account

--- src/examples/08_classes/01_bank/bank_account.cpp
+++ src/examples/08_classes/01_bank/bank_account.cpp
@@ -1,1 +1,35 @@
+#include "bank_account.h"
 //bank_account.cpp
+void BankAccount::get_balance_from_db()
+{   
+    balance = rand() % 10000 + 1;//value between 1 and 10000
+}
+
+void BankAccount::deposit(int amount)
+{
+    if(amount > 0)
+    {
+        balance += amount;
+    }
+}
+
+void BankAccount::withdraw(int amount)
+{
+    if(amount > 0 && amount <= balance)
+    {
+        balance -= amount;
+    }
+}
+
+//FREE FUNCTIONS; NOT PART OF THE CLASS!!!
+void show_balance(const BankAccount &account)
+{
+    std::cout<<"Friend balance: "<<account.balance<<"\n";
+}
+
+std::ostream& operator<<(std::ostream& out, const BankAccount &account)
+{
+    out<<"Overload balance: "<<account.balance<<"\n";
+
+    return out;
+}//bank_account.cpp
