e5bcb648914ee416bd8267858691b82203148ec4
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-23 02:04:12 +0000

    while exp

--- src/examples/04_repetition/01_while/while.cpp
+++ src/examples/04_repetition/01_while/while.cpp
@@ -3,7 +3,17 @@
 //the sum of all the squares of that number from 1 to the number. 
 //Example: sum_of_squares(4) 
 //1*1 + 2 * 2 + 3*3 + 4*4 = 30
+int sum_of_squares(int num)
+{   auto sum = 0;
 
+    while(num > 0 )
+    {
+        sum = num * num + sum;
+        num = num - 1; //decrement num
+
+    }
+    return sum;
+}
 
 //Using a while loop write code for display function that accepts a string and displays 
 //each character of the string in a line
