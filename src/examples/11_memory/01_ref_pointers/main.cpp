742cc16df5c1acc438109827abcb63a9f94ec651
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-23 02:09:48 +0000

    memory ref pointer and dynamic memory

--- src/examples/11_memory/01_ref_pointers/main.cpp
+++ src/examples/11_memory/01_ref_pointers/main.cpp
@@ -1,8 +1,39 @@
 #include "ref_pointers.h"
 #include<iostream>
 
+using std::cout;
+
 int main() 
 {
+	auto num = 5;
 	
+	int &num_ref = num;
+
+	cout<<num_ref<<"\n";
+	cout<<"Address num_ref references: "<<&num_ref<<"\n";
+	cout<<"Address of num: "<<&num<<"\n";
+
+	auto num1 = 10;
+	num_ref = num1;
+
+	cout<<num_ref<<"\n";
+	cout<<"Address num_ref references: "<<&num_ref<<"\n";
+	cout<<"Address of num: "<<&num<<"\n";
+	cout<<"Address of num1: "<<&num1<<"\n";
+
+	///Pointers
+	int *num_ptr = &num;//create and assign address to point to
+	cout<<"Address num_ptr points to: "<<num_ptr<<"\n";
+	cout<<"Value of num: "<<num<<"\n";
+	cout<<"Address of num: "<<&num<<"\n";
+
+	*num_ptr = 10;//work with the value of the variable address num_ptr points to
+	cout<<"Value of num: "<<num<<"\n";
+	
+	num_ptr = &num1;//assign address to point to
+	cout<<"Address of num1: "<<&num1<<"\n";
+	cout<<"Address num_ptr points to: "<<num_ptr<<"\n";
+	cout<<"Value at address that num_ptr points to: "<<*num_ptr<<"\n";
+
 	return 0;
 }
--- src/examples/11_memory/01_ref_pointers/ref_pointers.cpp
+++ src/examples/11_memory/01_ref_pointers/ref_pointers.cpp
@@ -1,4 +1,10 @@
 #include "ref_pointers.h"
 
 //
+void ref_ptr_params(int &num1, int *num2)
+{
+    num1 = 10;
+    *num2 = 20;//* means dereference the pointer
+               //change the value of the variable that num2 points to
+}
 
