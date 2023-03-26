1f8c04577ec8f6c72904978518684eaab64e90b3
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-26 02:21:10 +0000

    tictactoe.h

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,22 @@
 //h
+#include <string>
+#include <vector>
+
+
+
+class TicTacToe 
+{
+    public:
+        void start_game(std::string first_player);
+        void mark_board(int position);
+        void display_board() const;
+        bool game_over();
+    
+    private:
+        void set_next_player();
+        bool check_board_full();
+        void clear_board();
+        std::string player;
+        std::vector<std::string> pegs{9, " "};
+
+};
