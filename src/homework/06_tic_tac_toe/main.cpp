09682a43128a6bdb350bb48939a7eb6bd3b3a6aa
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-26 04:01:02 +0000

    tictactoe main

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,42 @@
+#include <iostream>
+#include <string>
+#include "tic_tac_toe.h"
+
+using std::cout; using std::cin; using std::string;
 
 int main() 
 {
+	TicTacToe game;
+	std::string first_player;
+	cout << "Enter the first player (X or O): ";
+	cin >> first_player;
+	game.start_game(first_player);
+
+	bool continue_game = true;
+	while (continue_game)
+	{
+		int position;
+		game.display_board();
+		cout << "Enter a position (1-9): ";
+		cin >> position;
+		game.mark_board(position);
+		if (game.game_over())
+		{
+		
+			cout << "Game over!\n";
+			std::string answer;
+			cout << "Play again? (Y/N): ";
+			cin >> answer;
+			if (answer == "N")
+			{
+				continue_game = false;
+			}
+			std::string first_player;
+			game.start_game(first_player);
+		}
+
+	}
+
 
 	return 0;
 }
