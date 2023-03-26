c87d60f56af08e5bbe1ad4f3cd178a23eebd10cd
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-26 02:22:54 +0000

    tictactoe test too many arugements

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,7 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "tic_tac_toe.h"
+
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test game over if 9 slots are selected")
+{
+	TicTacToe t;
+	t.start_game("X");
+	t.mark_board(1, 2, 3, 4, 5, 7, 6, 9, 8);
+	t.game_over();
+	
+
+	REQUIRE(t.game_over(true) == true);
+
+}
