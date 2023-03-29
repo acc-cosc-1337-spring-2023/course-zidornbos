39d961a2197646df1c23c505e3ecb2d7d1e6d3b7
zidornbos <123031057+zidornbos@users.noreply.github.com>
2023-03-29 22:23:55 +0000

    shape

--- src/examples/08_classes/02_shapes/main.cpp
+++ src/examples/08_classes/02_shapes/main.cpp
@@ -11,8 +11,23 @@ Create vector of Shape pointers
 iterate with auto
 */
 
+using std::unique_ptr; using std::make_unique; using std::vector;
+
 int main() 
 {
+	unique_ptr<Shape> shape = make_unique<Line>();
+	shape->draw();
+	unique_ptr<Shape> shape1 = make_unique<Circle>();
+	shape1->draw();
+
+	vector<unique_ptr<Shape>> shapes;
+	shapes.push_back(std::move(shape));
+	shapes.push_back(std::move(shape1));
+
+	for(auto& shape: shapes)
+	{
+		shape->draw();
+	}
 
 	return 0;
 }
--- src/examples/08_classes/02_shapes/shape.h
+++ src/examples/08_classes/02_shapes/shape.h
@@ -1,1 +1,13 @@
+#include<iostream>
+
 //shape.h
+#ifndef SHAPE_H
+#define SHAPE_H
+
+class Shape //is an abstract class
+{
+public:
+    virtual void draw() = 0;//pure virtual function
+};
+
+#endif
